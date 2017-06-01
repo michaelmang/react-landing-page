422ee1d8cfff301ef708edf49ebd899ab4107312
Michael Mangialardi <mikemangialardi94@gmail.com>
2017-05-31 23:07:27 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,10 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: './index.html',
   filename: 'index.html',
   inject: 'body'
-});
-const config = {
+})
+module.exports = {
   entry: './index.js',
   output: {
     filename: 'bundle.js'
@@ -25,15 +24,4 @@ const config = {
     ]
  },
  plugins: [HtmlWebpackPluginConfig]
-};
-
-if("production") {
-  config.plugins.push(
-    new webpack.DefinePlugin({
-      'process.env' = {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin()
-  )
 }
